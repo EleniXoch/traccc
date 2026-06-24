39d491a073eaf9c9e1f64ed42fdc30447253210b
exochell <eleni.xochelli@cern.ch>
2026-06-24 17:43:36 +0200

    added full tracking itk example

---
+++ extras/traccc_itk_example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+### please go ahead and edit the paths to the executable as needed
+### keep in mind you need access to atlas-eftracking data to run this script
+
+/eos/home-e/exochell/traccc/traccc_st_plots/traccc/build/bin/traccc_seq_example \
+   --detector-file="/eos/project/a/atlas-eftracking/GPU/ITk_data/latest/detray_detector_geometry.json" \
+   --material-file="/eos/project/a/atlas-eftracking/GPU/ITk_data/latest/detray_detector_material_maps.json" \
+   --grid-file="/eos/project/a/atlas-eftracking/GPU/ITk_data/latest/detray_detector_surface_grids.json" \
+   --digitization-file="/eos/project/a/atlas-eftracking/GPU/ITk_data/latest/ITk_digitization_config.json" \
+   --conditions-file="/eos/project/a/atlas-eftracking/GPU/ITk_data/latest/ITk_conditions_config.json" \
+   --read-bfield-from-file \
+   --bfield-file="/eos/project/a/atlas-eftracking/GPU/ITk_data/latest/ITk_bfield.cvf" \
+   --input-directory="/eos/project/a/atlas-eftracking/GPU/ITk_data/traccc_standalone_data/ttbar_mu200/" \
+   --use-acts-geom-source=0 \
+   --input-events=10
